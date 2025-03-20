433e1a21da771da4d02e000f2cfba0754780d426
kuldeepsinghra <19erecs035.kuldeep@rietjaipur.ac.in>
2025-03-20 12:56:36 +0530

    add nodemon in dev enviourment

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,7 +5,7 @@ const ProductController = require('../controllers/productController');
 const app = express.Router();
 
 // Routes using the class methods
-app.get('/',ProductController.getAllProducts);          // Get all products
+app.get('/', ProductController.getAllProducts);          // Get all products
 app.get('/:id', ProductController.getProductById);      // Get product by ID
 app.post('/', ProductController.createProduct);         // Create a new product
 app.delete('/:id', ProductController.deleteProduct);    // Delete product by ID
